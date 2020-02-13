cbd56c61708c17d56e205ac200caf0622d2e48ea
HaitongZhang <zhanghaitong01@corp.netnease.com>
2020-02-13 20:22:25 +0800

    Add text

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 window.onload = function() {
-      document.getElementById('main-content').innerHTML = 'Audio samples from "Unsupervised Learning For Improving Data Efficiency In Sequence-to-sequence Text-to-speech"'
-      document.getElementById('paper'.innerHTML = 'Paper  <a href="http://www.yahoo.com"> </a>'
+      document.getElementById('main-content').innerHTML = 'Audio samples from "Unsupervised Learning For Improving Data Efficiency In Sequence-to-sequence TTS"'
               )
       
 }
