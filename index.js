45f3f5d6eb55bc5c12730eee126f849bf3824388
HaitongZhang <zhanghaitong01@corp.netnease.com>
2020-02-13 20:31:43 +0800

    Add text

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-      document.getElementById('main-content').innerHTML = 'Audio samples from "Unsupervised Learning For Improving Data Efficiency In Sequence-to-sequence TTS"'
+      document.getElementById('main-content').innerHTML = 'Audio samples from "Unsupervised Learning For Improving Data Efficiency In Sequence-to-sequence Text-to-speech"'
               )
       
 }
