3498e0a3459166281212bf338dbb338d025af4d8
HaitongZhang <zhanghaitong01@corp.netnease.com>
2020-02-13 20:02:41 +0800

    Add a wav

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 window.onload = function() {
-      document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+      document.getElementById('main-content').innerHTML = 'Audio samples from "Unsupervised Learning For Improving Data Efficiency In Sequence-to-sequence Text-to-speech"'
+      document.getElementById('paper'.innerHTML = 'Paper  <a href="http://www.yahoo.com"> </a>'
+              )
+      
 }
